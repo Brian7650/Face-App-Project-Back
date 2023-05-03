99684d6f739ba3e5ea809ee501c5b62b8afd3ba0
Brian.B <brtl765@gmail.com>
2023-05-03 12:29:35 -0400

    update API

--- controllers/image.js
+++ controllers/image.js
@@ -1,12 +1,13 @@
 const Clarifai = require('clarifai');
+require('dotenv').config();
 
 const app = new Clarifai.App({
-   apiKey: '...Change api...'
+   apiKey: 'API KEY HERE'
  });
 
   const handleApiCall = (req, res) => {
     app.models
-      .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+      .predict('face-detection', req.body.input)
       .then(data => {
         res.json(data);
       })
