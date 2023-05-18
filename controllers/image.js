d8056e6f9d2efb2fbab5845b29df07af200e1424
Brian.B <brtl765@gmail.com>
2023-05-18 10:30:57 -0400

    regular API 2023

--- controllers/image.js
+++ controllers/image.js
@@ -1,72 +1,40 @@
-const {ClarifaiStub, grpc} = require("clarifai-nodejs-grpc");
+const Clarifai = require('clarifai');
 
-const stub = ClarifaiStub.grpc();
+//You must add your own API key here from Clarifai. 
+const app = new Clarifai.App({
+ apiKey: 'YOUR_API_KEY_HERE' 
+});
 
-const metadata = new grpc.Metadata();
-metadata.set("authorization", "Key YOUR_CLARIFAI_API_KEY");
-
-
-// const Clarifai = require('clarifai');
-require('dotenv').config();
 
 const handleApiCall = (req, res) => {
-  stub.PostModelOutputs(
-    {
-        // This is the model ID of a publicly available General model. You may use any other public or custom model ID.
-        model_id: "face-detection",
-        inputs: [{data: {image: {url: "req.body.input"}}}]
-    },
-    metadata,
-    (err, response) => {
-        if (err) {
-            console.log("Error: " + err);
-            return;
-        }
+  // HEADS UP! Sometimes the Clarifai Models can be down or not working as they are constantly getting updated.
+  // A good way to check if the model you are using is up, is to check them on the clarifai website. For example,
+  // for the Face Detect Mode: https://www.clarifai.com/models/face-detection
+  // If that isn't working, then that means you will have to wait until their servers are back up. 
 
-        if (response.status.code !== 10000) {
-            console.log("Received failed status: " + response.status.description + "\n" + response.status.details);
-            return;
-        }
-
-        console.log("Predicted concepts, with confidence values:")
-        for (const c of response.outputs[0].data.concepts) {
-            console.log(c.name + ": " + c.value);
-      }
-      res.json(response)
-  }
-);
-
-
-
+  app.models.predict('face-detection', req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
 }
 
+const handleImage = (req, res, db) => {
+  const { id } = req.body;
+  db('users').where('id', '=', id)
+  .increment('entries', 1)
+  .returning('entries')
+  .then(entries => {
+    // If you are using knex.js version 1.0.0 or higher this now returns an array of objects. Therefore, the code goes from:
+    // entries[0] --> this used to return the entries
+    // TO
+    // entries[0].entries --> this now returns the entries
+    res.json(entries[0].entries);
+  })
+  .catch(err => res.status(400).json('unable to get entries'))
+}
 
-// const app = new Clarifai.App({
-//    apiKey: 'YOUR_API_KEY'
-//  });
-
-//   const handleApiCall = (req, res) => {
-//     app.models
-//       .predict('face-detection', req.body.input)
-//       .then(data => {
-//         res.json(data);
-//       })
-//       .catch(err => res.status(400).json('unable to work with API'))
-// }
-
-
-//    const handleImage = (req, res, db) => {
-//     const { id } = req.body;
-//     db('users').where('id', '=', id)
-//     .increment('entries', 1)
-//     .returning('entries')
-//     .then(entries => {
-//       res.json(entries[0].entries);
-//     })
-//     .catch(err => res.status(400).json('unable to get entries'))
-//   }
-  
-  module.exports = {
-    // handleImage,
-    handleApiCall
-  }
+module.exports = {
+  handleImage,
+  handleApiCall
+}
